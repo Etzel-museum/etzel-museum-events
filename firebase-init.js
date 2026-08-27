aed5eb4c01e8e5f3646cb5b93df08fa16c8111a7
david <davidlevi674@gmail.com>
2026-08-27 10:38:16 +0300

    Sort sessions by a single dateTime field instead of two orderBy clauses
    Firestore requires a manually-created composite index for compound
    orderBy queries; a single combined field avoids that setup step
    entirely. Wired in the real Firebase project config.

--- firebase-init.js
+++ firebase-init.js
@@ -2,14 +2,13 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/fireba
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-firestore.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
 
-// TODO: paste the config object from Firebase Console -> Project settings -> Your apps
 const firebaseConfig = {
-  apiKey: "PASTE_ME",
-  authDomain: "PASTE_ME",
-  projectId: "PASTE_ME",
-  storageBucket: "PASTE_ME",
-  messagingSenderId: "PASTE_ME",
-  appId: "PASTE_ME"
+  apiKey: "AIzaSyByseHcR_-cXW8MTQfC11SR3MDqJwhewLM",
+  authDomain: "etzel-museum-events.firebaseapp.com",
+  projectId: "etzel-museum-events",
+  storageBucket: "etzel-museum-events.firebasestorage.app",
+  messagingSenderId: "792662745222",
+  appId: "1:792662745222:web:5149dac5004c1c7fceb107"
 };
 
 export const app = initializeApp(firebaseConfig);
